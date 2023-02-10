48d7fbda57d44069ea11379fcdfa4af84b937333
xumm <xumm@csoneplus.com>
2023-02-10 09:35:12 +0800

    add veye_mvcam-cm4-overlay.dts for 5.10.103

--- driver_source/dts/rpi-5.10.103+/build_dtbo.sh
+++ driver_source/dts/rpi-5.10.103+/build_dtbo.sh
@@ -3,3 +3,4 @@ dtc -@ -Hepapr -I dts -O dtb -o csimx307.dtbo csimx307-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o cssc132.dtbo cssc132-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o veyecam2m.dtbo veyecam2m-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o veyecam2m-cm4.dtbo veyecam2m-cm4-overlay.dts
+dtc -@ -Hepapr -I dts -O dtb -o veye_mvcam-cm4.dtbo veye_mvcam-cm4-overlay.dts
