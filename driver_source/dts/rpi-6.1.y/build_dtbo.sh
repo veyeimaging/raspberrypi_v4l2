fd0f45c24294ea605c8b2a4be3a896c0da96d410
xumm <xumm@csoneplus.com>
2023-06-07 11:22:03 +0800

    add fpdlink dirver

--- driver_source/dts/rpi-6.1.y/build_dtbo.sh
+++ driver_source/dts/rpi-6.1.y/build_dtbo.sh
@@ -3,4 +3,5 @@ dtc -@ -Hepapr -I dts -O dtb -o csimx307.dtbo csimx307-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o cssc132.dtbo cssc132-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o veyecam2m.dtbo veyecam2m-overlay.dts
 dtc -@ -Hepapr -I dts -O dtb -o veye_vbyone.dtbo veye_vbyone-overlay.dts
+dtc -@ -Hepapr -I dts -O dtb -o ds90ub954.dtbo ds90ub954-overlay.dts
 
